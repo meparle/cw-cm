0f6ce5b916935e27e61dc9fff668ae9d821c10d3
Eileen Parle <eileenlny@gmail.com>
2017-03-12 14:35:09 +0000

    MeetingImpl tests (via PastMeetingImpl instance) with getId failing

--- src/test/java/test/MeetingImplTest.java
+++ src/test/java/test/MeetingImplTest.java
@@ -3,8 +3,11 @@ package test;
 import impl.MeetingImpl;
 import impl.PastMeetingImpl;
 import org.junit.Test;
+import spec.Contact;
 
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,12 +17,14 @@ import static org.junit.Assert.assertEquals;
 public class MeetingImplTest {
 
     @Test
-    public void test_didMyNotesGetRecorded() {
-//        Calendar cal = Calendar.getInstance();
-//        String input = "Bob sounds interested.";
-//        PastMeetingImpl testMI = new PastMeetingImpl(5, cal - month, "Bob", input);
-//        String output = testMI.getNotes();
-//        String expected = * + "Bob sounds interested.";
-//        assertEquals(expected, output);
+    public void test_getId() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -1);
+        Set<Contact> contacts = new HashSet<>();
+        int input = 5;
+        PastMeetingImpl testMI = new PastMeetingImpl(input, cal, contacts, "Everything's coming up Milhouse");
+        int output = testMI.getId();
+        int expected = 5;
+        assertEquals(expected, output);
     }
 }
